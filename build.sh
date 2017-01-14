82aa7f87d014ee54969950cf3108b7dd3f0f9372
Mohammad Rahhal <m.r992@hotmail.com>
2017-01-14 15:20:22 +0200

    chore: fix

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 dotnet --info
 dotnet restore
-dotnet test test/MR.Augmenter.Tests -f netcoreapp1.0
-dotnet test test/MR.Augmenter.AspNetCore.Tests -f netcoreapp1.0
+dotnet test test/MR.Augmenter.Tests -f netcoreapp1.1
+dotnet test test/MR.Augmenter.AspNetCore.Tests -f netcoreapp1.1
