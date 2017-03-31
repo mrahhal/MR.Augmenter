7b8a10df47118d378f2040d7a01f3c159b4c3d60
Mohammad Rahhal <m.r992@hotmail.com>
2017-03-31 18:22:23 +0300

    Upgrade to dotnet tooling 1.0

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 dotnet --info
 dotnet restore
-dotnet test test/MR.Augmenter.Tests -f netcoreapp1.1
-dotnet test test/MR.Augmenter.AspNetCore.Tests -f netcoreapp1.1
+dotnet test test/MR.Augmenter.Tests/MR.Augmenter.Tests.csproj -f netcoreapp1.1
+dotnet test test/MR.Augmenter.AspNetCore.Tests/MR.Augmenter.AspNetCore.Tests.csproj -f netcoreapp1.1
