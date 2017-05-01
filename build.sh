463f56d96abd7e6ef28828946b6f3c7e67cc904a
Mohammad Rahhal <m.r992@hotmail.com>
2017-05-01 11:12:07 +0300

    Fix osx build?

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
+header "Adjusting file descriptors limit, if necessary"
+FILE_DESCRIPTOR_LIMIT=$( ulimit -n )
+if [ $FILE_DESCRIPTOR_LIMIT -lt 512 ]
+then
+	info "Increasing file description limit to 512"
+	ulimit -n 512
+fi
+
 dotnet --info
 dotnet restore
 dotnet test test/MR.Augmenter.Tests/MR.Augmenter.Tests.csproj -f netcoreapp1.1
