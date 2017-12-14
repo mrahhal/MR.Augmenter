32b7f8cd93d8e1177b4d60801195550d195671fe
Mohammad Rahhal <m.r992@hotmail.com>
2017-12-14 11:37:23 +0200

    Fix build.sh

--- build.sh
+++ build.sh
@@ -8,5 +8,5 @@ fi
 
 dotnet --info
 dotnet restore
-dotnet test test/MR.Augmenter.Tests/MR.Augmenter.Tests.csproj -f netcoreapp1.1
-dotnet test test/MR.Augmenter.AspNetCore.Tests/MR.Augmenter.AspNetCore.Tests.csproj -f netcoreapp1.1
+dotnet test test/MR.Augmenter.Tests/MR.Augmenter.Tests.csproj -f netcoreapp2.0
+dotnet test test/MR.Augmenter.AspNetCore.Tests/MR.Augmenter.AspNetCore.Tests.csproj -f netcoreapp2.0
