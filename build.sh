15c651c21c43e73716cdfce21217729a9d54e865
Mohammad Rahhal <m.r992@hotmail.com>
2017-12-14 16:40:31 +0200

    * build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
-header "Adjusting file descriptors limit, if necessary"
-FILE_DESCRIPTOR_LIMIT=$( ulimit -n )
-if [ $FILE_DESCRIPTOR_LIMIT -lt 512 ]
-then
-	info "Increasing file description limit to 512"
-	ulimit -n 512
-fi
+#header "Adjusting file descriptors limit, if necessary"
+#FILE_DESCRIPTOR_LIMIT=$( ulimit -n )
+#if [ $FILE_DESCRIPTOR_LIMIT -lt 512 ]
+#then
+#	info "Increasing file description limit to 512"
+#	ulimit -n 512
+#fi
 
 dotnet --info
 dotnet restore
